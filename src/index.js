fa86938b0d45fe538351ef6f9bc1fba505e5d969
Andy Wendt <andy@awendt.com>
2016-01-13 05:21:23 -0700

    Function invocation examples completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+function functionInvocation() {
+    return 'function invocation';
+}
+
+function functionInvocationThisReturn() {
+    return this;
+}
+
+describe("Function Invocation", function() {
+    it("It can be called as a function", function() {
+        expect(functionInvocation()).toBe('function invocation');
+    });
+
+    it("It is attached to the window object and thus global", function() {
+        expect(window.functionInvocation()).toBe('function invocation');
+    });
+
+    it("It's 'this' equals the window object", function() {
+        expect(functionInvocationThisReturn()).toBe(window);
     });
 });
