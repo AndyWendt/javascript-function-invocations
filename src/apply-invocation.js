b7bded16e16b7beb043aa755ac9df7f12f89fc1b
Andy Wendt <andy@awendt.com>
2016-01-13 21:41:09 -0700

    added Call and Apply Invocation examples

--- src/apply-invocation.js
+++ src/apply-invocation.js
@@ -1,3 +1,16 @@
 describe("Apply() Invocation", function() {
+    var obj = {score: 500};
+    var obj2 = {score: 5000};
 
+    function test(person, descriptive) {
+         return person + ' scored ' + this.score + ' and that is ' + descriptive;
+    }
+
+    it("will have a `this` context of obj", function() {
+        expect(test.apply(obj, ['johnny','horrible'])).toBe('johnny scored 500 and that is horrible');
+    });
+
+    it("will have a `this` context of obj2", function() {
+        expect(test.apply(obj2, ['jimmy', 'great'])).toBe('jimmy scored 5000 and that is great');
+    });
 });
--- src/call-invocation.js
+++ src/call-invocation.js
@@ -1,4 +1,16 @@
 describe("Call() Invocation", function() {
+    var obj = {score: 500};
+    var obj2 = {score: 5000};
 
+    function test(person, descriptive) {
+        return person + ' scored ' + this.score + ' and that is ' + descriptive;
+    }
 
+    it("will have a `this` context of obj", function() {
+        expect(test.call(obj, 'johnny','horrible')).toBe('johnny scored 500 and that is horrible');
+    });
+
+    it("will have a `this` context of obj2", function() {
+        expect(test.call(obj2, 'jimmy', 'great')).toBe('jimmy scored 5000 and that is great');
+    });
 });
