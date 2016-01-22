1c049a6b5a62a24606690bf04389873875689e56
Andy Wendt <andy@awendt.com>
2016-01-21 22:10:01 -0700

    added function scope tests

--- src/function-scope.js
+++ src/function-scope.js
@@ -2,8 +2,15 @@ describe("Global Variables are awesome", function() {
 
     var a = 10;
 
+    // This is function hoisting :)
+    one();
+
     function one() {
         b = 15;
+
+        // This is function hoisting :)
+        two();
+
         function two() {
             var c = 20;
             it("I can see a, b and c", function() {
@@ -15,15 +22,10 @@ describe("Global Variables are awesome", function() {
             expect(a + b).toBe(25);
             expect(typeof c == 'undefined').toBe(true);
         });
-
-        two();
     }
 
     it("I can see a, b but not c", function() {
         expect(a + b).toBe(25);
         expect(typeof c == 'undefined').toBe(true);
     });
-
-
-    one();
 });
