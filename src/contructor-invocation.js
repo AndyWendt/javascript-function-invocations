35b3517513ca7702ae91c7ec018b6e4162cc76fd
Andy Wendt <andy@awendt.com>
2016-01-21 22:41:34 -0700

    automatically return this example

--- src/contructor-invocation.js
+++ src/contructor-invocation.js
@@ -1,10 +1,13 @@
 describe("Constructor Invocation", function() {
     var obj = new BaseConstructor();
     var obj1 = new BaseConstructor();
+    var returned = new ReturnThis();
 
     function BaseConstructor () {
         this.run = function () { return this; };
     }
+
+    function ReturnThis() { this.a = 50; }
 
     it("will have a `this` context of obj", function() {
         expect(obj.run()).toBe(obj);
@@ -12,5 +15,9 @@ describe("Constructor Invocation", function() {
 
     it("will have a `this` context of obj2", function() {
         expect(obj1.run()).toBe(obj1);
+    });
+
+    it("will automatically return this", function() {
+        expect(returned.a).toBe(50);
     });
 });
